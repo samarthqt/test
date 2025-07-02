d9476bafcb43eca1b8c2b842b3d6fcb51b1138fd
Samarth S <samarth.s@qualitestgroup.com>
2025-07-02 13:41:18 +0530

    Add automation output

---
+++ selenium1/test_scripts/FindPartnerTest.java
@@ -0,0 +1,30 @@
+package com.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import com.pageobjects.FindPartnerPage;
+import static org.testng.Assert.assertTrue;
+
+public class FindPartnerTest {
+
+    private FindPartnerPage findPartnerPage;
+
+    @BeforeMethod
+    public void setUp() {
+        findPartnerPage = new FindPartnerPage();
+        findPartnerPage.openBrowser();
+    }
+
+    @Test
+    public void testFindPartner() {
+        findPartnerPage.navigateToFindPartnerPage();
+        findPartnerPage.clickFindPartnerButton();
+        assertTrue(findPartnerPage.isPartnerPageDisplayed(), "Partner page is not displayed.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        findPartnerPage.closeBrowser();
+    }
+}
