d044a82a337736cda46ea0982d1ddc7081068092
Samarth S <samarth.s@qualitestgroup.com>
2025-07-24 12:40:29 +0530

    Add automation output

---
+++ src/main/java/com/qt/pshop/pageobjs/LoginPage.java
@@ -0,0 +1,25 @@
+package com.qt.pshop.pageobjs;
+import org.openqa.selenium.By;
+import com.framework.reusable.WebReusableComponents;
+
+public class LoginPage extends WebReusableComponents {
+
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("loginButton");
+    private By dashboard = By.id("dashboard");
+
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        enterText(usernameField, username);
+        enterText(passwordField, password);
+        clickElement(loginButton);
+    }
+
+    public boolean isDashboardDisplayed() {
+        return isElementVisible(dashboard, 5);
+    }
+}
---
+++ src/main/java/com/qt/pshop/pageobjs/OrdersPage.java
@@ -0,0 +1,40 @@
+package com.qt.pshop.pageobjs;
+import org.openqa.selenium.By;
+import com.framework.reusable.WebReusableComponents;
+
+public class OrdersPage extends WebReusableComponents {
+
+    private By ordersModule = By.id("ordersModule");
+    private By orderDetails = By.id("orderDetails");
+    private By shipmentStatusDropdown = By.id("shipmentStatus");
+    private By dispatchedOption = By.xpath("//option[text()='Dispatched']");
+
+    public OrdersPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void navigateToOrdersModule() {
+        clickElement(ordersModule);
+    }
+
+    public boolean isOrdersModuleDisplayed() {
+        return isElementVisible(ordersModule, 5);
+    }
+
+    public void selectOrderById(String orderId) {
+        By order = By.xpath("//tr[td[text()='" + orderId + "']]");
+        clickElement(order);
+    }
+
+    public boolean isOrderDetailsDisplayed(String orderId) {
+        return isElementVisible(orderDetails, 5);
+    }
+
+    public void updateShipmentStatusToDispatched() {
+        selectByVisibleText(shipmentStatusDropdown, "Dispatched");
+    }
+
+    public boolean isShipmentStatusUpdatedToDispatched() {
+        return getSelectedOption(shipmentStatusDropdown).equals("Dispatched");
+    }
+}
